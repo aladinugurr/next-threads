3d6eec5f8a428f947e001a771f2aba836a0f089d
Uğur <augurcatal@gmail.com>
2023-12-13 16:30:43 +0300

    activities added

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,30 +1,31 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose, { mongo } from "mongoose";
 
 const threadSchema = new mongoose.Schema({
-   text: {type: String, required: true},
-   author: {
+  text: { type: String, required: true },
+  author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
     required: true,
-},
-community: {
+  },
+  community: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Community'
-},
-createdAt: {
+    ref: "Community",
+  },
+  createdAt: {
     type: Date,
-    default: Date.now
-},
-parentId: {
-    type: String
-},
-children: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Thread'
-}]
+    default: Date.now,
+  },
+  parentId: {
+    type: String,
+  },
+  children: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+});
 
-})
-
-const Thread = mongoose.models.Thread || mongoose.model('Thread', threadSchema);
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
 
 export default Thread;
